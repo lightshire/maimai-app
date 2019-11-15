9b18c31f3ed9c0e622472f8a4cadde8a19948b38
JC Ricaro <jcricaro@gmail.com>
2019-11-15 14:25:18 +0800

    Delete entire app

--- screens/index.ts
+++
@@ -1,9 +0,0 @@
-import AuthSignupScreen from "./auth.signup";
-import AppHomeScreen from "./app.home";
-import AuthOTPScreen from "./auth.otp";
-
-export default [
-    { screen:  AuthSignupScreen, name: "auth.signup" },
-    { screen: AppHomeScreen, name: "app.home" },
-    { screen: AuthOTPScreen, name: "auth.otp" }
-];
--- utilities/branding/colors.ts
+++
@@ -1,7 +0,0 @@
-
-export default {
-    mairoon: "#8F1F1E",
-    gray: "#828282",
-    lightGray: "#E0E0E0",
-    maired: "#E9233A"
-};
